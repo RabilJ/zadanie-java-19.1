2b9db8a3cbbc3bfda7b6cd1bbe8b4f84d2fdc87d
RabilJ <jakub.popielarski7@gmail.com>
2018-11-15 17:36:11 +0100

    zadanie 19.1 druga wersja

--- src/TaskMethod.java
+++ src/TaskMethod.java
@@ -1,8 +1,9 @@
+import java.security.Key;
 import java.util.*;
 
 public class TaskMethod {
 
-    public static int whichNumAppearsMostTimes(List<Integer> list) {
+    public static Integer whichNumAppearsMostTimes(List<Integer> list) {
         Map<Integer, Integer> map1 = new HashMap<>();
         if (list == null) {
             throw new NoSuchElementException();
@@ -11,17 +12,9 @@ public class TaskMethod {
                 map1.put(number, Collections.frequency(list, number));
             }
         }
-            Set<Map.Entry<Integer, Integer>> set = map1.entrySet();
-            System.out.println(set.toString());
-            int maxValueInMap = Collections.max(map1.values());
-            int bestKey = 0;
-            List<Integer>list1 = new ArrayList<>();
-            for (Map.Entry<Integer, Integer> entry : set) {
-                if (entry.getValue() == maxValueInMap)
-                    list1.add(entry.getKey());
-            }
-            bestKey = Collections.min(list1);
-
-        return bestKey;
+        Set<Map.Entry<Integer, Integer>> set = map1.entrySet();
+        System.out.println(set.toString());
+        Integer key = Collections.max(set, Map.Entry.comparingByValue()).getKey();
+        return key;
     }
 }
